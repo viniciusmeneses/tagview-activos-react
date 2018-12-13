5ddd2c0614b7bcca415fa067acfc4678107ecf59
viniciusmeneses <vini23hot@gmail.com>
2018-12-13 16:06:54 -0200

    Added handler input functions

--- src/components/Active/index.js
+++ src/components/Active/index.js
@@ -1,32 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import './styles.css';
 
-const Active = ({
-  id, name, money, percent, removeActive, portfolio
-}) => (
-  <tr>
-    <td>{name}</td>
-    <td>
-      <div className="active-money">
-        <span>R$</span>
-        {' '}
-        <input type="number" name="active-money" value={money} />
-      </div>
-    </td>
-    <td>
-      <div className="active-percent">
-        <input type="number" name="active-percent" value={percent} />
-        {' '}
-        <span>%</span>
-      </div>
-    </td>
-    <td>
-      <button type="button" className="remove-button" onClick={() => removeActive(portfolio, id)}>
-        <i className="fas fa-times" />
-      </button>
-    </td>
-  </tr>
-);
+export default class Active extends Component {
+  state = {
+    moneyInput: this.props.money,
+    percentInput: this.props.percent,
+  };
 
-export default Active;
+  static propTypes = {
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    money: PropTypes.string.isRequired,
+    percent: PropTypes.string.isRequired,
+    removeActive: PropTypes.func.isRequired,
+    portfolio: PropTypes.number.isRequired,
+  };
+
+  handleMoneyInput = (e) => {
+    this.setState({ moneyInput: e.target.value });
+  };
+
+  handlePercentInput = (e) => {
+    this.setState({ percentInput: e.target.value });
+  };
+
+  render() {
+    const {
+      id, name, removeActive, portfolio,
+    } = this.props;
+    const { moneyInput, percentInput } = this.state;
+
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>
+          <div className="active-money">
+            <span>R$</span>
+            {' '}
+            <input
+              type="number"
+              name="active-money"
+              onChange={this.handleMoneyInput}
+              value={moneyInput}
+            />
+          </div>
+        </td>
+        <td>
+          <div className="active-percent">
+            <input
+              type="number"
+              name="active-percent"
+              onChange={this.handlePercentInput}
+              value={percentInput}
+            />
+            {' '}
+            <span>%</span>
+          </div>
+        </td>
+        <td>
+          <button
+            type="button"
+            className="remove-button"
+            onClick={() => removeActive(portfolio, id)}
+          >
+            <i className="fas fa-times" />
+          </button>
+        </td>
+      </tr>
+    );
+  }
+}
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,77 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Active from '../Active';
 
 import './styles.css';
 
-const Portfolio = ({ data, addActive, removeActive }) => (
-  <table className="actives-table">
-    <thead>
-      <tr>
-        <th>
-            Ativos (
-          <span>{data.actives.length}</span>
-)
-        </th>
-        <th>
-          <div className="total-money">
-            <span>R$</span>
-            <input type="number" name="total-money" value={data.totalMoney} />
-          </div>
-          <div className="sub-total-money">
-            <sub>
-                (Restante:
-              <span id="total-money-remaining">-720,43</span>
-)
-            </sub>
-          </div>
-        </th>
-        <th>
-          <strong className="total-percent">
-            <span>{data.totalPercent}</span>
-            {' '}
-%
-          </strong>
-          <sub className="sub-total-percent">Participação Iguais</sub>
-        </th>
-        <th />
-      </tr>
-    </thead>
-    <tbody>
-      {data.actives.map(active => (
-        <Active key={active.id} {...active} portfolio={data.id} removeActive={removeActive} />
-      ))}
-    </tbody>
-    <tfoot>
-      <tr>
-        <td colSpan="4">
-          <button
-            type="button"
-            className="add-button"
-            onClick={() => addActive.pushActive(data.id, addActive.createActive())}
-          >
-            <i className="fas fa-plus-square" />
-            {' '}
-Adicionar ativo
-          </button>
-        </td>
-      </tr>
-    </tfoot>
-  </table>
-);
+export default class Portfolio extends Component {
+  state = {
+    totalMoneyInput: this.props.data.totalMoney,
+  }
 
-Portfolio.propTypes = {
-  data: PropTypes.shape({
-    id: PropTypes.number,
-    totalMoney: PropTypes.string,
-    totalPercent: PropTypes.string,
-    actives: PropTypes.array,
-  }).isRequired,
-  addActive: PropTypes.shape({
-    createActive: PropTypes.func,
-    pushActive: PropTypes.func,
-  }).isRequired,
-  removeActive: PropTypes.func.isRequired,
-};
+  static propTypes = {
+    data: PropTypes.shape({
+      id: PropTypes.number,
+      totalMoney: PropTypes.string,
+      totalPercent: PropTypes.string,
+      actives: PropTypes.array,
+    }).isRequired,
+    addActive: PropTypes.shape({
+      createActive: PropTypes.func,
+      pushActive: PropTypes.func,
+    }).isRequired,
+    removeActive: PropTypes.func.isRequired,
+  };
 
-export default Portfolio;
+  handleTotalMoneyChange(e) {
+    this.setState({ totalMoneyInput: e.target.value })
+  }
+
+  render() {
+    const { data, addActive, removeActive } = this.props;
+    const { totalMoneyInput } = this.state;
+
+    return (
+      <table className="actives-table">
+        <thead>
+          <tr>
+            <th>
+                Ativos (
+              <span>{data.actives.length}</span>
+    )
+            </th>
+            <th>
+              <div className="total-money">
+                <span>R$</span>
+                <input type="number" name="total-money" onChange={this.handleTotalMoneyChange} value={totalMoneyInput} />
+              </div>
+              <div className="sub-total-money">
+                <sub>
+                    (Restante:
+                  <span id="total-money-remaining">-720,43</span>
+    )
+                </sub>
+              </div>
+            </th>
+            <th>
+              <strong className={`total-percent ${data.totalPercent === '100.00' ? 'total-percent-100' : ''}`}>
+                <span>{data.totalPercent}</span>
+                {' '}
+    %
+              </strong>
+              <sub className="sub-total-percent">Participação Iguais</sub>
+            </th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {data.actives.map(active => (
+            <Active key={active.id} {...active} portfolio={data.id} removeActive={removeActive} />
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="4">
+              <button
+                type="button"
+                className="add-button"
+                onClick={() => addActive.pushActive(data.id, addActive.createActive())}
+              >
+                <i className="fas fa-plus-square" />
+                {' '}
+    Adicionar ativo
+              </button>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    );
+  }
+}
